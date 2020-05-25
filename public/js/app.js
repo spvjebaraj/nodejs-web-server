5755762bcb05666b3688cae4144cfee23281e0d7
Philip Jebaraj <pv.jebaraj@gmail.com>
2020-05-25 19:32:43 +0530

    Removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Loading from app.js");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
